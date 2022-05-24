e8eae0e774a71bd28c029c7a116106ed4165d60c
karanpokar <karanpatel2199@gmail.com>
2022-05-24 16:35:51 +0530

    Updates

---
+++ precache-manifest.c393790ee06c34870977382bee104e95.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c38be06a22ee2dc1dcd07b0b601eef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8db7616c94ca2e366d43",
+    "url": "/static/css/main.534816e6.chunk.css"
+  },
+  {
+    "revision": "b2a5779790c9c63c7012",
+    "url": "/static/js/2.43516913.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/static/js/2.43516913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db7616c94ca2e366d43",
+    "url": "/static/js/main.4d9a49a5.chunk.js"
+  },
+  {
+    "revision": "81d87c671834291546fa",
+    "url": "/static/js/runtime-main.d6fa9567.js"
+  },
+  {
+    "revision": "91a25158a77627d787c62a71b627483f",
+    "url": "/static/media/Firewires.91a25158.png"
+  },
+  {
+    "revision": "98fb7b9001fef721965f33f1cded6a9b",
+    "url": "/static/media/Picaday.98fb7b90.png"
+  },
+  {
+    "revision": "17547e8c7099c45e410a2dfa74d36b44",
+    "url": "/static/media/barko13p.17547e8c.png"
+  },
+  {
+    "revision": "44d11d87b9ef1c73c96b9790d447da5c",
+    "url": "/static/media/covhelp.44d11d87.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "268be14f1f4d0e46c2bde0e95a16998d",
+    "url": "/static/media/features-split-image-01.268be14f.png"
+  },
+  {
+    "revision": "1da8d950e3f30a69d7c957c26238e1de",
+    "url": "/static/media/firebase.1da8d950.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "29c015fae1b6e91ccc2b244d1558212f",
+    "url": "/static/media/javaScript.29c015fa.svg"
+  },
+  {
+    "revision": "53d0c384a48f09cd1f9c7299baef4941",
+    "url": "/static/media/python.53d0c384.svg"
+  },
+  {
+    "revision": "de6d6eafd0264b8c667b2042ad261989",
+    "url": "/static/media/react-native.de6d6eaf.svg"
+  },
+  {
+    "revision": "92a4fd2ecc1d3fb9154ffd270481b342",
+    "url": "/static/media/redux.92a4fd2e.svg"
+  },
+  {
+    "revision": "83a5879ce55899e4643f486b76f0f6f1",
+    "url": "/static/media/tensorflow.83a5879c.svg"
+  }
+]);
